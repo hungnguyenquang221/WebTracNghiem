3ae51ac1c85c1f4bc6b79a2f4ea889cdd3c76f72
Vo Hieu Nghia <nvohieeunghia@gmail.com>
2023-02-28 22:34:38 +0700

    Design Homepage

---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+      <Container>
+        <NavLink to='/'  className='navbar-brand'>TracNghiem</NavLink>
+        <Navbar.Toggle   aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to='/'       className='nav-link'>Trang Chủ</NavLink>
+            <NavLink to='/users'  className='nav-link'>Người Dùng</NavLink>
+            <NavLink to='/admins' className='nav-link'>Admin</NavLink>
+          </Nav>
+            <Nav>
+              <button>Log in</button>
+              <button>Sign up</button>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item >Log in</NavDropdown.Item>
+              <NavDropdown.Item >Log out</NavDropdown.Item>
+              <NavDropdown.Item >Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    )
+}
+
+export default Header;
