b318547d6c54163133ad3772708dbcac537a814b
hungnguyenquang221 <hungnguyenquang221@gmail.com>
2023-03-23 22:38:57 +0700

    Add ManagerUser.scss

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,17 +1,18 @@
+import ModalCreateUser from "./ModalCreateUser";
+import "./ManagerUser.scss";
 const ManageUser = (props) => {
-    return(
-        <div className="manage-user-container">
-            <div className="title">
-                    Manage User
-            </div>
-            <div className="users-content">
-                <button> Add new user</button>
-            </div>
-            <div>
-                table users
-            </div>
+  return (
+    <div className="manage-user-container">
+      <div className="title">Manage User</div>
+      <div className="users-content">
+        <div>
+          <button> Add new user</button>
         </div>
-    )
-}
+        <div>table users</div>
+      </div>
+      <ModalCreateUser />
+    </div>
+  );
+};
 
 export default ManageUser;
