0633be29130320e0788872d087a8d4c9288293dc
hungnguyenquang221 <95635825+hungnguyenquang221@users.noreply.github.com>
2023-03-29 22:59:45 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { 
-BrowserRouter,
-Routes,
-Route, 
-} from "react-router-dom";
-import User from './components/User/User'; 
-import HomePage from './components/Home/HomePage';
-import Admin from './components/Admin/Admin';
-import ManageUser from './components/Admin/Content/ManageUser';
-import DashBoard from './components/Admin/Content/DashBoard';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./components/User/User";
+import HomePage from "./components/Home/HomePage";
+import Admin from "./components/Admin/Admin";
+import ManageUser from "./components/Admin/Content/ManageUser";
+import DashBoard from "./components/Admin/Content/DashBoard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} >
+        <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
-          <Route path="users" element={<User />} />     
-          
+          <Route path="users" element={<User />} />
         </Route>
-        <Route path="/admins" element={<Admin />} >
-           <Route index element={<DashBoard />} />
-          <Route path="manage-users" element={<ManageUser />} />   
-          </Route>  
-        </Routes>
-      </BrowserRouter>
+        <Route path="/admins" element={<Admin />}>
+          <Route index element={<DashBoard />} />
+          <Route path="manage-users" element={<ManageUser />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
     {/* </React.StrictMode> */}
   </Provider>
